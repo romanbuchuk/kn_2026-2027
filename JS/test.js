ca1fce24d8cea3b56d64a80ffe2e52f7d96a8b51
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2026-09-10 11:51:03 +0300

    initial commit

---
+++ JS/test.js
@@ -0,0 +1,19 @@
+// Declare a variable
+
+// const;
+// let;
+// var;
+
+// const is used to declare a constant variable
+// let is used to declare a variable that can be reassigned
+// var is used to declare a variable that can be reassigned
+
+let name = "John";
+console.log(typeof name);
+name = false;
+console.log(typeof name);
+name = 56;
+console.log(typeof name);
+name = function () {};
+console.log(typeof name);
+// name = [1, 'sd', true];
