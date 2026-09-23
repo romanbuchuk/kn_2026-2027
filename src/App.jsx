0ed2a3f90154ab7a0a70a9490d5ad2d2ef92c83e
Roman Buchuk <roman.buchuk@sigmatechnology.com>
2026-09-23 12:34:09 +0300

    add semicolon for testing purpose

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from './assets/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -116,7 +116,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
